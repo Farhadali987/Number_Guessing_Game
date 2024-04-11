2a9fb5890e8feaa3c02affb8e88a46d098d53e0b
Farhad gul <164610570+Farhadali987@users.noreply.github.com>
2024-04-11 01:03:22 -0700

    Add files via upload
    This is simple CLI Number Guessing Game.

---
+++ index.js
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+let random = Math.floor(Math.random() * 10 + 1);
+let ran = await inquirer.prompt([
+    {
+        message: "Enter your random number between 1-6:",
+        name: "guess",
+        type: "number",
+    },
+]);
+if (ran.guess === random) {
+    console.log("congratulations you entered right number");
+    console.log("Author: Farhad Ali Laghari.");
+}
+else {
+    console.log("You entered wrong number");
+}
